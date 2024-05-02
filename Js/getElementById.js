5f5ee0024766184b78f31d58506f607248a4df6c
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-02 17:18:24 +0530

    How to manipulate element by using Id also we can give style using id of that element

---
+++ Js/getElementById.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("box2").innerHTML = "This text is shown using JS";
+// document.getElementById("box2").innerText = "This text is shown using JS";   
+
+document.getElementById("box1").style.backgroundColor = "green";
+document.body.style.backgroundColor = "violet";
+
+document.getElementById("box2").style.backgroundColor = "Yellow";
+document.getElementById("box2").style.textAlign = "center";
