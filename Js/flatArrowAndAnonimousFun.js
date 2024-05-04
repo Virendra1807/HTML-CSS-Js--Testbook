b912507c6c4eeab98577eeaae3df692b632169a7
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-04 23:17:09 +0530

    Flat arrow and Anonimous function in Js

---
+++ Js/flatArrowAndAnonimousFun.js
@@ -0,0 +1,12 @@
+// Anonymous Function
+let greet = function(name){
+    console.log("Hello " + name + ", I'm anonymous function");
+};
+greet("viren");
+
+
+// Flat Arrow Function
+let greeting = (nm) =>{
+    console.log("Hello "+ nm + ", I'm flat arrow function")
+};
+greeting("Viren Mali");
