479ceb6f728b2e7d6a2544e2599b58e1b178a2ce
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-05 12:50:13 +0530

    Types of Loop : For/in/of, While, Do-while

---
+++ Js/typesOfLoops.js
@@ -0,0 +1,20 @@
+
+// Printing each character of name
+var nm =  "viren";
+for( character of nm){
+    console.log(character);
+}
+
+console.log("");
+
+// declaring function for addition
+function add(a=0 ,b=0){
+    var c = a + b;
+    return c;
+}
+var result  = add(10, 20);
+console.log(result);
+
+
+
+
