45af064d799388a365a9a9f754e30d8feee212d9
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-06 20:35:06 +0530

    Anonymous and Arrow functions in JS

--- Js/flatArrowAndAnonimousFun.js
+++ Js/flatArrowAndAnonimousFun.js
@@ -1,4 +1,5 @@
-// Anonymous Function
+
+// Anonymous Function ( Before flat arrow function )
 let greet = function(name){
     console.log("Hello " + name + ", I'm anonymous function");
 };
@@ -10,3 +11,15 @@ let greeting = (nm) =>{
     console.log("Hello "+ nm + ", I'm flat arrow function")
 };
 greeting("Viren Mali");
+
+
+// arrow function
+let add = (x, y) =>{
+    return x+y;
+};
+console.log(add(20, 20));
+
+// flat arrow function returns without return keyword, Here don't use Curly brackets.
+let sum = (a, b) => a+b;
+console.log(sum(20, 20));
+
