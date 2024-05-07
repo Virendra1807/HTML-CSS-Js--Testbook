e1ccf3cbad1582b0e0f449d37ac3e2235ade1b1c
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-07 16:49:44 +0530

    Binding function and How Var gives wrong output when we use setTimeout Method

--- Js/bindingInJs.js
+++ Js/bindingInJs.js
@@ -20,8 +20,24 @@ animal.print();
 let birdBind = animal.print.bind(bird); 
 birdBind();
 
-// By using Bind we can use function of another object
+// // By using Bind we can use function of another object
 
+// When we use setTimeout it is necessary to bind the function otherwise it will give wrong output
+//There are 2 solutions : 
+// 1) Don't use Var bcoz Var is global variable instead of that use Let which has Local Scope
 
+for (let i = 1; i <= 5; i++) {
+        setTimeout(function () {
+            console.log("printing " + i + " after " + i + " seconds")
+        }, i * 1000);
+}
+// 2) Or we can use immediate invoked function expression (Function Closure )
 
+for (var i = 1; i <= 5; i++) {
+    (function (i) {
+        setTimeout(function () {
+            console.log("printing " + i + " after " + i + " seconds")
+        }, i * 1000);
+    })(i);
+}
 
