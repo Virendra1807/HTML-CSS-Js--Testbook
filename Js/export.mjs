78111a1d58a7609966acb295edd8876e27b1e56f
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-10 17:48:30 +0530

    How to Import and Export modules, While importing and exporting we need to give extension of file as '.mjs' or need to add 'type : 'modeule' in package.json

---
+++ Js/export.mjs
@@ -0,0 +1,9 @@
+
+export const add = (a, b) => a+b;
+const subtract = (a, b) => a-b;
+
+// or we can export like below : 
+
+export {
+    subtract as sub
+}
---
+++ Js/import.mjs
@@ -0,0 +1,6 @@
+import { add, sub } from './export.mjs';
+
+console.log(add(10, 20));
+console.log(sub(60,10));
+
+
