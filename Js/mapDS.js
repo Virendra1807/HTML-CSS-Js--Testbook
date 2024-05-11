7729ae87ffca5631c0acab9887ee605f6ee415cf
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-11 17:21:47 +0530

    Map data structure in js

--- Js/mapDS.js
+++ Js/mapDS.js
@@ -1,6 +1,28 @@
 
 let stud1 = new Map();
 
-stud1.put('name', 'viren');
-stud1.put('marks', 86);
+stud1.set('name', 'viren');
+stud1.set('marks', 86);
+stud1.set('class', 'B');
+stud1.set('gender', 'M');
+stud1.set('status', 'Fail');
 
+console.log(stud1);
+
+console.log(stud1.get('name'));
+
+stud1.delete('status');
+console.log(stud1);
+stud1.set('status', 'pass');
+console.log(stud1);
+
+
+console.log(stud1.size);
+
+stud1.forEach(function(value, key){
+    console.log(key+ " : "+ value);
+});
+
+
+console.log(stud1.keys());
+console.log(stud1.values());
