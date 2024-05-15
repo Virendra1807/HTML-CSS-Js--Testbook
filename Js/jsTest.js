5ad5c80417156916aca761ca870d95a22acf20d6
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-15 12:19:29 +0530

    String methods

--- Js/jsTest.js
+++ Js/jsTest.js
@@ -1,8 +1,8 @@
-//String Methods in Js
-// let s = "Hello Viren";
-// console.log(s.repeat(5));
-// console.log(s.length);
-// console.log(s.replace("Viren", "Kiran"));
+// String Methods in Js
+let s = "Hello Viren";
+console.log(s.repeat(5));
+console.log(s.length);
+console.log(s.replace("Viren", "Kiran"));
 // console.log(s.split("e"));
 // console.log(s.endsWith("ren"));
 
