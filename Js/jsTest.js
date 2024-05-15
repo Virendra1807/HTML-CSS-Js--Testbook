ae8c2fa15063d740553f4708503be4e85a976568
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-15 12:19:55 +0530

    String methods

--- Js/jsTest.js
+++ Js/jsTest.js
@@ -3,8 +3,8 @@ let s = "Hello Viren";
 console.log(s.repeat(5));
 console.log(s.length);
 console.log(s.replace("Viren", "Kiran"));
-// console.log(s.split("e"));
-// console.log(s.endsWith("ren"));
+console.log(s.split("e"));
+console.log(s.endsWith("ren"));
 
 
 
