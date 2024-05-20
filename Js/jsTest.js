994641e96bc7cf7a0108f7bc34a9951f6d688c40
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-05-20 14:37:35 +0530

    test

--- Js/jsTest.js
+++ Js/jsTest.js
@@ -1,10 +1,10 @@
 // String Methods in Js
 let s = "Hello Viren";
-// console.log(s.repeat(5));
-// console.log(s.length);
-// console.log(s.replace("Viren", "Kiran"));
-// console.log(s.split("e"));
-// console.log(s.endsWith("ren"));
+console.log(s.repeat(5));
+console.log(s.length);
+console.log(s.replace("Viren", "Kiran"));
+console.log(s.split("e"));
+console.log(s.endsWith("ren"));
 
 
 
