286579453355458591b2acfede10aa4cee383672
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-06-11 19:02:19 +0530

    Obj methods

--- test.js
+++ test.js
@@ -1,7 +1,22 @@
 // <<<<<<< HEAD
-console.log("Helllo World");
-console.log("Helllo World");
-console.log("Changes this Js code from GitHub and Pull it to local syatem");
+// console.log("Helllo World");
+// console.log("Helllo World");
+// console.log("Changes this Js code from GitHub and Pull it to local syatem");
 // >>>>>>> ade9cae2b3d30d1d83b984dffd34dd9f1c54b8b3
 
 // automatically Addded this file while pulling JS code from GitHub
+
+var car ={
+    'name': 'Hyundai Creta',
+    'milege': 19,
+    'price': 1800000,
+    'wheels': 4,
+    'Number': 3129
+}
+
+car.milege = '17kmpl';
+console.log(car);
+Object.seal(car);
+car.milege = '20kmpl';
+// car.number = 3129;
+console.log(car);
