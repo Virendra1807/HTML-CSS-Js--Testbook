8710f2a3a46060e583ab20873b566e8b8ed46a9a
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-06-28 13:29:28 +0530

    HTTP Methods GET, POST, PUT, PATCH, DELETE

--- Js/API/httpVerbs.js
+++ Js/API/httpVerbs.js
@@ -5,7 +5,7 @@ const putBtn = document.getElementById('put-btn');
 const patchBtn = document.getElementById('patch-btn');
 const deleteBtn = document.getElementById('delete-btn');
 
-//GET method
+//GET method for getting Data
 getBtn.addEventListener('click', async () => {
     try {
         const responce = await fetch('https://catfact.ninja/fact');
@@ -20,3 +20,80 @@ getBtn.addEventListener('click', async () => {
         console.log(error);
     }
 });
+
+//POST method for Creating post
+postBtn.addEventListener('click', async () => {
+    try {
+        // Syntax for post method
+        const responce = await fetch('https://catfact.ninja/fact', {
+            method: "POST",
+            body: "BODY OF POST",
+            headers: ""
+        });
+
+        if (responce.ok) {
+            let data = await responce.json();
+            let facts = document.getElementById('facts');
+            facts.innerText = data.fact
+        } else {
+            throw new Error(`Something Wrong, Network Status : ${responce.status}`);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+//PUT method for Updating complete post
+putBtn.addEventListener('click', async () => {
+    try {
+        const responce = await fetch('https://catfact.ninja/fact');
+        if (responce.ok) {
+            let data = await responce.json();
+            let facts = document.getElementById('facts');
+            facts.innerText = data.fact
+        } else {
+            throw new Error(`Something Wrong, Network Status : ${responce.status}`);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//PATCH method for Updating a single property not a complete post
+patchBtn.addEventListener('click', async () => {
+    try {
+        const responce = await fetch('https://catfact.ninja/fact');
+        if (responce.ok) {
+            let data = await responce.json();
+            let facts = document.getElementById('facts');
+            facts.innerText = data.fact
+        } else {
+            throw new Error(`Something Wrong, Network Status : ${responce.status}`);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+//DELETE method for Deleting post from API
+deleteBtn.addEventListener('click', async () => {
+    try {
+        const responce = await fetch('https://catfact.ninja/fact');
+        if (responce.ok) {
+            let data = await responce.json();
+            let facts = document.getElementById('facts');
+            facts.innerText = data.fact
+        } else {
+            throw new Error(`Something Wrong, Network Status : ${responce.status}`);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+
+
+
+
