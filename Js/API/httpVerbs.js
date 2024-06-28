f632d12142837ce8bcde1356eadddd47b5140e81
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-06-28 13:22:36 +0530

    HTTP Methods GET, POST, PUT, PATCH, DELETE

--- Js/API/httpVerbs.js
+++ Js/API/httpVerbs.js
@@ -1,19 +1,22 @@
 
-
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 const putBtn = document.getElementById('put-btn');
 const patchBtn = document.getElementById('patch-btn');
 const deleteBtn = document.getElementById('delete-btn');
 
-var url = 'https://catfact.ninja/fact';
 //GET method
-getBtn.addEventListener('click', ()=>{
-    fetch(url)
-    .then((res)=> res.json())
-    .then((data)=> {
-        let facts = document.getElementById('facts');
-        facts.innerText = data.fact
-    })
-    .catch((err)=> console.error(err));
+getBtn.addEventListener('click', async () => {
+    try {
+        const responce = await fetch('https://catfact.ninja/fact');
+        if (responce.ok) {
+            let data = await responce.json();
+            let facts = document.getElementById('facts');
+            facts.innerText = data.fact
+        } else {
+            throw new Error(`Something Wrong, Network Status : ${responce.status}`);
+        }
+    } catch (error) {
+        console.log(error);
+    }
 });
