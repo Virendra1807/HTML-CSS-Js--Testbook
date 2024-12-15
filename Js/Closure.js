12152c82be683940ce677bce69c3eb0981f80769
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-12-15 17:07:50 +0530

    Closure in JS

--- Js/Closure.js
+++ Js/Closure.js
@@ -14,3 +14,33 @@ let a = outer();
 a();
 a();
 a();
+
+
+// SCOPE
+function a(){
+    let age = 18; //Global scope
+    function b(){
+        console.log(age); // thats why we access age
+    }
+    b();
+}
+
+a();
+
+// Closure
+
+function outer2(){
+    let age = 23; 
+    return function inner2(){
+        age++;
+        console.log(age);
+    }
+}
+
+let res = outer2();
+
+res();
+res();
+res();
+
+
