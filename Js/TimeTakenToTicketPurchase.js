ac9e3793885db5a64c11402d1cba4d9a2c69cedc
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-12-22 19:30:36 +0530

    Time taken to buy all tickets to kth person

--- Js/TimeTakenToTicketPurchase.js
+++ Js/TimeTakenToTicketPurchase.js
@@ -8,24 +8,24 @@ function TimeTakenToBuyTicket(){
     for(let i=0; i<ticketQueue.length; i++){
         
         if(ticketQueue[i] <= ticketQueue[k] && i<=k  ){
+            // Person requires less than kth persons so simply add tickets requirement of them
                 TimeTaken += ticketQueue[i];
         }
         else if(ticketQueue[i] > ticketQueue[k] && i<=k){
             // Add ticketQueue[k]th tickets because they want more tickets than kth person 
-            // so they will be in queue after kth person buys his all tickets
+            // so they will be in queue after kth person to buys all tickets
             TimeTaken += ticketQueue[k];
         }
         else if (ticketQueue[i] < ticketQueue[k] && i>k){
+            // add person tickets if he wants less than kth person 
             TimeTaken += ticketQueue[i];
         }
         else{
-            // Add ticketQueue[k]th tickets because they want more tickets than kth person 
+            // Add ticketQueue[k]th - 1 tickets because they want more tickets than kth person 
             // so they will be in queue after kth person buys his all tickets
             TimeTaken += ticketQueue[k]-1;
         }
-    
     }
-
     console.log('Time taken to buy all tickets to kth person is ' + TimeTaken);
 }
 
